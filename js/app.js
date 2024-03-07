d61b95c1fcb4bc829b70017f4f61f384a0fff16d
Nerk <josephhuamanilol@gmail.com>
2024-03-07 15:44:57 -0500

    Add new images, SVG icons, and slider functionality

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ const menu = document.querySelector('.nav__menu');
 const menulist = document.querySelector('.nav__list');
 const links = document.querySelectorAll('.nav__link');
 
+
+
 menu.addEventListener('click', () => {
     menulist.classList.toggle('nav__list--show')
 });
@@ -11,3 +13,44 @@ links.forEach(function(link){
         menulist.classList.remove('nav__list--show');
     });
 });
+
+
+let slider = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let dots = document.querySelectorAll('.slider .dots li');
+
+let lengthItems = items.length - 1;
+let active = 0;
+next.onclick = function(){
+    active = active + 1 <= lengthItems ? active + 1 : 0;
+    reloadSlider();
+}
+prev.onclick = function(){
+    active = active - 1 >= 0 ? active - 1 : lengthItems;
+    reloadSlider();
+}
+let refreshInterval = setInterval(()=> {next.click()}, 3000);
+function reloadSlider(){
+    slider.style.left = -items[active].offsetLeft + 'px';
+    // 
+    let last_active_dot = document.querySelector('.slider .dots li.active');
+    last_active_dot.classList.remove('active');
+    dots[active].classList.add('active');
+
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(()=> {next.click()}, 3000);
+
+    
+}
+
+dots.forEach((li, key) => {
+    li.addEventListener('click', ()=>{
+         active = key;
+         reloadSlider();
+    })
+})
+window.onresize = function(event) {
+    reloadSlider();
+};
